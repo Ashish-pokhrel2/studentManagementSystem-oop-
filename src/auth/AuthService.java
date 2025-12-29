9c0deccd7b4b3f4a3c9542a1d991acd4792724b1
Ashish-pokhrel2 <aashish.pokhrel.12122@gmail.com>
2025-12-29 14:06:08 +0545

    AuthService class create

---
+++ src/auth/AuthService.java
@@ -0,0 +1,18 @@
+package auth;
+
+public class AuthService {
+	
+	boolean authenticate(String username, String password) {
+		
+		return false;
+		
+	}
+	
+	boolean changePassword(int userId, String oldPassword, String
+	newPassword) {
+		
+		return false;
+		
+		}
+
+}
